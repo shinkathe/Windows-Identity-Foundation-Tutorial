67414679d3ad08c18de15eeb19ca9d0a84519305
laritu <tuomisto.lari@gmail.com>
2014-03-02 15:36:36 +0200

    Added STS and made the Forms Authentication app use it

---
+++ Learning SSO/STS/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IdentityModel;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Services;
+using System.Diagnostics;
+
+namespace STS.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        //
+        // GET: /Authentication/
+        [Route("")]
+        public void Index()
+        {
+            var wsFederationMessage = WSFederationMessage.CreateFromUri(Request.Url);
+            Trace.Write(String.Join(",", wsFederationMessage.Parameters.Select(i => i.Key + " - " + i.Value)));
+        }
+	}
+}
