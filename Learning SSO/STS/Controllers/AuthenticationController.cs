486bd0c3b75890ca4956822133e26c6237743562
shinpou <tuomisto.lari@gmail.com>
2014-12-18 00:29:48 +0200

    Do not require certificate to be installed, and fix a few bugs

--- Learning SSO/STS/Controllers/AuthenticationController.cs
+++ Learning SSO/STS/Controllers/AuthenticationController.cs
@@ -9,7 +9,6 @@ using System.Security.Claims;
 using System.IdentityModel.Configuration;
 using System.IdentityModel.Tokens;
 using System.Security.Cryptography.X509Certificates;
-using STS.Util;
 using STS.Core;
 using System.Security.Principal;
 
@@ -20,6 +19,8 @@ namespace STS.Controllers
         public List<string> LogoutUrls;
         public string ReplyTo;
     }
+
+
     
     public class AuthenticationService {
         public const string SignOutLiteral = "wsignout1.0";
@@ -27,30 +28,46 @@ namespace STS.Controllers
         public const string SignInLiteral = "wsignin1.0";
         public const string RealmLiteral = "wtrealm";
         public const string ReplyLiteral = "wreply";
+
+        // Load certificate
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\Certificate.pfx", AppDomain.CurrentDomain.BaseDirectory), "");
+        }
+
         /// <summary>
         /// User hadn't logged in before, so set an authentication cookie in the WSFed response for this domain
         /// </summary>
-        /// 
-        private IPrincipal AuthenticateAndCreateCookie(string realm)
+        private IPrincipal AuthenticateAndCreateCookie(string relyingPartyUrl)
         {
             var user = new ClaimsIdentity(AuthenticationTypes.Federation);
             user.AddClaim(new Claim(ClaimTypes.Name, "test"));
-            user.AddClaim(new Claim(ClaimTypes.Uri, realm));
-
-            var claimsPrincipal = new ClaimsPrincipal(user);
-            var sessionSecurityToken = new SessionSecurityToken(claimsPrincipal, TimeSpan.FromDays(365));
-            FederatedAuthentication.SessionAuthenticationModule.WriteSessionTokenToCookie(sessionSecurityToken);
-            return claimsPrincipal;
+            user.AddClaim(new Claim(ClaimTypes.Uri, relyingPartyUrl));
+            return SaveToCookie(user);
         }
 
         /// <summary>
         /// The user had already signed in from one domain, sign the user in from the previously set cookie
         /// </summary>
-        private IPrincipal PreviouslyAuthenticated(string realm)
+        private IPrincipal PreviouslyAuthenticated(string relyingPartyUrl)
         {
-            var user = new ClaimsIdentity(AuthenticationTypes.Federation);
-            user.AddClaim(new Claim(ClaimTypes.Uri, realm));
-            return new ClaimsPrincipal(user);;
+            var user = (ClaimsIdentity)HttpContext.Current.User.Identity;
+            user.AddClaim(new Claim(ClaimTypes.Uri, relyingPartyUrl));
+            return SaveToCookie(user);
+        }
+
+        /// <summary>
+        /// Save user state to cookie
+        /// </summary>
+        /// <param name="user">User to save to cookie</param>
+        /// <returns></returns>
+        private static ClaimsPrincipal SaveToCookie(ClaimsIdentity user)
+        {
+            var claimsPrincipal = new ClaimsPrincipal(user);
+            var sessionSecurityToken = new SessionSecurityToken(claimsPrincipal, TimeSpan.FromDays(365));
+            FederatedAuthentication.SessionAuthenticationModule.WriteSessionTokenToCookie(sessionSecurityToken);
+            return claimsPrincipal;
         }
 
         /// <summary>
@@ -59,23 +76,35 @@ namespace STS.Controllers
         /// <returns></returns>
         public ActionResult ProcessRequest()
         {
+            // Pull the request apart
             var message = WSFederationMessage.CreateFromUri(HttpContext.Current.Request.Url);
-            var realm = message.GetParameter(RealmLiteral);
+            // Get the relying party url
+            var relyingPartyUrl = HttpContext.Current.Request.UrlReferrer.ToString();
+            // Get reply to address
             var reply = message.GetParameter(ReplyLiteral);
-            return message.Action == SignOutLiteral ? SignOut(reply) : SignIn(realm);
+            // Sign out, if the action or wa-parameter is "wsignout1.0", and sign in otherwise
+            return message.Action == SignOutLiteral ? SignOut(reply, relyingPartyUrl) : SignIn(relyingPartyUrl);
         }
-        
+
         /// <summary>
         /// Find out which realms the user is signed in - sign them out from all of them, and return to @replyTo
         /// </summary>
         /// <param name="replyTo">Redirect to this address after signout is done</param>
+        /// <param name="relyingPartyUrl"></param>
         /// <returns>A bit of html, which renders images with signout urls for all domains.</returns>
-        private ActionResult SignOut(string replyTo)
+        private ActionResult SignOut(string replyTo, string relyingPartyUrl)
         {
+            // First, remove the session authentication cookie for the STS
             FederatedAuthentication.SessionAuthenticationModule.SignOut();
             var ci = (ClaimsIdentity)HttpContext.Current.User.Identity;
+            // Get all urls where the user has signed in previously, and make them into a list of strings with the format "{url}?wa=wsignoutcleanup1.0"
             var logoutUrls = ci.FindAll(i => i.Type == ClaimTypes.Uri).Select(i => string.Format("{0}?wa={1}", i.Value, SignOutCleanupLiteral)).ToList();
+            // Construct a viewmodel from the logout urls and replyto address
             var model = new LogoutViewModel { LogoutUrls = logoutUrls, ReplyTo = replyTo };
+            // Add relying party url if it isn't in there, because in some cases, a client might call signout even though the local STS cookie has expired 
+            var relyingPartyUrlCleanup = string.Format("{0}?wa={1}", relyingPartyUrl, SignOutCleanupLiteral);
+            if (!logoutUrls.Contains(relyingPartyUrlCleanup)) logoutUrls.Add(relyingPartyUrlCleanup);
+            // Build a viewresult object and return that
             var viewResult = new ViewResult
             {
                 ViewName = "~/Views/Shared/Logout.cshtml",
@@ -84,10 +113,10 @@ namespace STS.Controllers
             return viewResult;
         }
 
-        private ActionResult SignIn(string realm)
+        private ActionResult SignIn(string replyToAddress)
         {
-            var user = HttpContext.Current.User.Identity.IsAuthenticated ? PreviouslyAuthenticated(realm) : AuthenticateAndCreateCookie(realm);
-            var config = new SecurityTokenServiceConfiguration("http://sts.local", new X509SigningCredentials(CertificateUtil.GetCertificate(StoreName.My, StoreLocation.LocalMachine, "CN=learningSSO")));
+            var user = HttpContext.Current.User.Identity.IsAuthenticated ? PreviouslyAuthenticated(replyToAddress) : AuthenticateAndCreateCookie(replyToAddress);
+            var config = new SecurityTokenServiceConfiguration("http://sts.local", new X509SigningCredentials(LoadCertificate()));
 
             FederatedPassiveSecurityTokenServiceOperations.ProcessRequest(HttpContext.Current.Request, (ClaimsPrincipal) user, new CustomTokenService(config), HttpContext.Current.Response);
             return new HttpStatusCodeResult(HttpStatusCode.OK);
